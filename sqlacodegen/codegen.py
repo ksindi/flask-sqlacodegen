941c3818c9664057346e568683b2b103db7ae448
ksindi <ksindi@ksindi.com>
2014-07-28 16:16:00 -0400

    big fix when rendering cachables

--- sqlacodegen/codegen.py
+++ sqlacodegen/codegen.py
@@ -352,9 +352,9 @@ class ModelClass(Model):
     def render(self):
         text = 'class {0}({1}):\n'.format(self.name, self.parent_name)
 
-        text += 'cache_label = {0!r}\n'.format('default')
-        text += 'cache_regions = regions\n'
-        text += 'query_class = query_callable(regions)\n'
+        text += '    cache_label = {0!r}\n'.format('default')
+        text += '    cache_regions = regions\n'
+        text += '    query_class = query_callable(regions)\n'
 
         text += '    __tablename__ = {0!r}\n'.format(self.table.name)
 
